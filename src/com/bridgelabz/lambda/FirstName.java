9ccaf20be013c1e9c6b401a03b84f29b8405fd41
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-29 23:46:06 +0530

    UC1 First Name

---
+++ src/com/bridgelabz/lambda/FirstName.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.lambda;
+
+import java.util.regex.Pattern;
+
+public interface FirstName {
+    void validUser(String name);
+}
+class RegexLambdaExpression {
+
+    public static <Firstname> void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+        Firstname function = (name) -> {
+            boolean check = Pattern.matches("([A-Z][a-z]{3,})", name);
+            if (check == true) {
+                System.out.println("Firstname is Valid");
+            } else
+                System.out.println("Enter valid name");
+        };
+        function.validUser("sarfaraz");
+    }
+}
