c44ecb8ef1b322c58d30565e00bac42213e9df47
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-30 23:14:51 +0530

    UC8 Password rule 4

---
+++ src/com/bridgelabz/lambda/PasswordRule4.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.lambda;
+
+import java.util.regex.Pattern;
+@FunctionalInterface
+interface PasswordRule4 {
+    void validUser(String password);
+}
+class RegexLambdaExpression7 {
+
+    public static void main(String[] args) {
+        PasswordRule4 function = (password)->{
+            boolean check = Pattern.matches("^(?=.*[0-9]{1,})(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$",password);
+            if (check == true) {
+                System.out.println("Password is Valid");
+            }else
+                System.out.println("Password Is Invalid");
+        };
+        function.validUser("Praju@1234");
+    }
+}
